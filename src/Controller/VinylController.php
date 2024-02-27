5f183692d2ccfa6c8e88ebe73dcf416074ca3092
aadaams <adam.smida01@gmail.com>
2024-02-27 11:07:35 +0100

    Added some Tiwggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController
+class VinylController extends AbstractController
 {
-    #[Route("/page1")]
+    #[Route("/")]
     public function homepage(): Response
     {
-        return new Response("Bonjour <strong> SMIDA-Adam</strong> ");
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'ISET KELIBIA',
+            'tracks' => $tracks,
+        ]);
     }
     #[Route("/browse/{slug}")]
     function browse(string $slug = null): Response
